3e1d05ad14608a03d3aaf2883105415493dd0969
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2024-01-09 11:12:11 +0300

    styled the page

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -2,19 +2,28 @@ import React, {useState} from 'react'
 
 function CurrencyConverter() {
 const[convertedAmount, setConvertedAmount] = useState("This is where the converted amount value will go")
+
+function handleCurrencyConversion(){
+    setConvertedAmount("This is the converted amount")
+}
   return (
     <>
-    <div>
+    <div id='page1'>
         <h1>Currency converter</h1>
-        <input type='number' id='amount'></input>
-        <select>
-            <option value="option">Currency from</option>
-        </select>
-        <select>
-            <option value="option">Currency to</option>
-        </select>
-        <button id='convert'>Convert</button>
-        <div>{convertedAmount}</div>
+        <div className='amount'>
+            <label for = "amount">Enter amount</label><br></br>
+            <input type='number' id='amount'></input>
+        </div>
+        <div id='select'>
+            <select>
+                <option value="option">Currency from</option>
+            </select>
+            <select>
+                <option value="option">Currency to</option>
+            </select>
+        </div>
+        <button id='convert'onClick={handleCurrencyConversion}>Convert</button>
+        <div id='converted'>{convertedAmount}</div>
     </div>
     </>
   )
