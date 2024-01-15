81d6f4a637586d8b0e3ed098bf92656daebafc70
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2024-01-15 15:05:25 +0300

    made changes to the fetch request

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -1,11 +1,39 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function CurrencyConverter() {
+
+const [rates, setRates] = useState();
+const [ratesFetched, setRatesFetched] = useState();
 const[convertedAmount, setConvertedAmount] = useState("This is where the converted amount value will go")
 
+useEffect(()=>{
+    const URL = "https://v6.exchangerate-api.com/v6/0ba10b681bdf7772f0f7c96a/latest/USD"
+// fetching the rates from the API
+    const fetchRates = async()=>{
+        const response = await fetch(URL)
+        .then((response) => response.json());
+
+// saving the rates in the state
+if(response.result === "success"){
+    setRates(response.conversion_rates);
+    setRatesFetched(true);
+}
+    }
+}, [])
+//         try{
+//             const response = await fetch(URL);
+//             const json = await response.json();
+//             console.log(json);
+//         } catch(error){
+//             console.log("erro", error)
+//         }
+//     }
+//     fetchData();
+// },[])
 function handleCurrencyConversion(){
     setConvertedAmount("This is the converted amount")
 }
+console.log(ratesFetched)
   return (
     <>
     <div id='page1'>
@@ -15,9 +43,17 @@ function handleCurrencyConversion(){
             <input type='number' id='amount'></input>
         </div>
         <div id='select'>
-            <select>
-                <option value="option">Currency from</option>
-            </select>
+            <select id="from">
+        {ratesFetched ? (
+          Object.keys(rates).map((currency, index) => (
+            <option key={index} value={currency}>
+              {currency}
+            </option>
+          ))
+        ) : (
+          <option defaultValue>USD</option>
+        )}
+      </select>
             <select>
                 <option value="option">Currency to</option>
             </select>
